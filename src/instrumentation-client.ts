8d09c9830cedbb039d21bca64e234d99cf56dd1c
Abbas Hirani <156702136+AbbasHirani@users.noreply.github.com>
2026-08-07 03:18:18 +0000

    chore: add person_profiles setting to PostHog client init

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -3,6 +3,7 @@ import posthog from 'posthog-js'
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     defaults: '2026-05-30',
+    person_profiles: 'identified_only',
     capture_pageview: false, // We handle this manually in AnalyticsWrapper
     disable_session_recording: true, // We handle this manually in AnalyticsWrapper
     autocapture: {
